6311fa032998d19e47aa8d26c71f85cb2583b3d9
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-04-23 20:17:36 +0700

    update login.js file

--- js/login.js
+++ js/login.js
@@ -2,14 +2,17 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     event.preventDefault(); // Отменяем стандартное поведение формы
 
     const formData = new FormData(this); // Получаем данные формы
+    const errorMessage = document.getElementById('error-msg')
 
     const response = await fetch(this.action, {
         method: this.method,
         body: formData
     });
-
+    if (response.status == 401){
+        errorMessage.style.display = 'block';
+        return;
+    }
     const data = await response.json(); // Преобразуем ответ в JSON
-
     console.log(data); // Выводим полученный JSON в консоль
     document.cookie = `auth_token=${data['access_token']}; path=/`
     window.location.href="admin-chat.html"
